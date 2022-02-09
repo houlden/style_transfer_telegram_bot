afc4bc3dd2052d5852090c446f44b979d7170b54
houlden <houlden105350@gmail.com>
2022-02-10 00:32:45 +0300

    added load state dict of vgg19 (only first five convs)

--- ml_models/nst_gatys/run_style_transfer.py
+++ ml_models/nst_gatys/run_style_transfer.py
@@ -1,7 +1,8 @@
+import torch
 import torchvision.models as models
 
 from ml_models.nst_gatys.image_preparation import image_loader, save_image
-from ml_models.nst_gatys.settings import DEVICE, NORMALIZATION_MEAN, NORMALIZATION_STD
+from ml_models.nst_gatys.settings import DEVICE, NORMALIZATION_MEAN, NORMALIZATION_STD, CNN_WEIGHTS_PATH
 from ml_models.nst_gatys.train_loop import style_transfer_loop
 
 
@@ -13,7 +14,11 @@ async def run(style_path, content_path, output_path):
     normalization_mean = NORMALIZATION_MEAN.to(DEVICE)
     normalization_std = NORMALIZATION_STD.to(DEVICE)
 
-    vgg19 = models.vgg19(pretrained=True).features.to(DEVICE).eval()
+    # Чтобы не грузить каждый раз веса всей VGG19 (~600 МБ) загрузим только саму сеть без весов и веса первых 11-ти
+    # слоев до 5-й свертки включительно (~2 МБ)
+    vgg19 = models.vgg19(pretrained=False).features.to(DEVICE).eval()
+    vgg19_first_five_conv_layers = torch.load(CNN_WEIGHTS_PATH, map_location=DEVICE)
+    vgg19[:11].load_state_dict(vgg19_first_five_conv_layers)
 
     output_image = await style_transfer_loop(vgg19, normalization_mean, normalization_std, content_image, style_image,
                                              input_image)
--- ml_models/nst_gatys/settings.py
+++ ml_models/nst_gatys/settings.py
@@ -1,11 +1,27 @@
 import torch
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-IMAGE_SIZE = 128  # Размер выходного изображения IMAGE_SIZE*IMAGE_SIZE
-CONTENT_LAYERS = ['conv_4']  # Слои, по которым считается content-loss
-STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']  # Слои, по которым считается style-loss
-NORMALIZATION_MEAN = torch.tensor([0.485, 0.456, 0.406])  # Средние значения, по которым нормировалась VGG19
-NORMALIZATION_STD = torch.tensor([0.229, 0.224, 0.225])  # Стандартные отклонения, по которым нормировалась VGG19
-NUM_STEPS = 500  # Количество итераций оптимизации
-STYLE_WEIGHT = 100000  # Вес style-loss
-CONTENT_WEIGHT = 1  # Вес content-loss (небольшой вес, поскольку на вход сети подается копия изображения content)
+
+# Размер выходного изображения IMAGE_SIZE*IMAGE_SIZE
+IMAGE_SIZE = 128
+
+# Слои, по которым считается content-loss
+CONTENT_LAYERS = ['conv_4']
+# Слои, по которым считается style-loss
+STYLE_LAYERS = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+# Первые 11 слоев (5 сверток VGG19)
+CNN_WEIGHTS_PATH = 'ml_models/nst_gatys/pretrained_models/vgg19_first_five_conv_layers.pth'
+
+# Средние значения, по которым нормировалась VGG19
+NORMALIZATION_MEAN = torch.tensor([0.485, 0.456, 0.406])
+# Стандартные отклонения, по которым нормировалась VGG19
+NORMALIZATION_STD = torch.tensor([0.229, 0.224, 0.225])
+
+# Количество итераций оптимизации
+NUM_STEPS = 500
+
+# Вес style-loss
+STYLE_WEIGHT = 100000
+# Вес content-loss (небольшой вес, поскольку на вход сети подается копия изображения content)
+CONTENT_WEIGHT = 1
