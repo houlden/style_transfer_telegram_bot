02056d87be79ac84bd025d6aeddc06e40e87940c
houlden <houlden105350@gmail.com>
2022-02-13 18:13:22 +0300

    refactored webhook parameters

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
-from os import getenv
+import os
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-ADMIN_ID = getenv('ADMIN_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMIN_ID = os.getenv('ADMIN_ID')
 
-WEBHOOK_URL = getenv('WEBHOOK_URL')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+WEBHOOK_PATH = os.getenv('WEBHOOK_PATH')
+HOST = os.getenv('HOST')
+PORT = int(os.environ.get('PORT', 5000))
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-
 from bot_init import bot, dp, storage
 from aiogram import executor
-from config import ADMIN_ID, WEBHOOK_URL
+from config import ADMIN_ID, WEBHOOK_URL, WEBHOOK_PATH, HOST, PORT
 import handlers  # Запуск хэндлеров
 
 
@@ -23,9 +21,9 @@ if __name__ == '__main__':
     #                        on_shutdown=on_shutdown,
     #                        skip_updates=True)
     executor.start_webhook(dispatcher=dp,
-                           webhook_path='',
+                           webhook_path=WEBHOOK_PATH,
                            on_startup=on_startup,
                            on_shutdown=on_shutdown,
                            skip_updates=True,
-                           host='0.0.0.0',
-                           port=int(os.environ.get('PORT', 5000)))
+                           host=HOST,
+                           port=PORT)
