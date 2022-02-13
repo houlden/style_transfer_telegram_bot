a5f52cd58628f39c2abc9fc773e28202ad17bc71
houlden <houlden105350@gmail.com>
2022-02-13 13:25:04 +0300

    added on_shutdown-function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from bot_init import bot, dp
+from bot_init import bot, dp, storage
 from aiogram import executor
 from config import ADMIN_ID
-import handlers
+import handlers  # Запуск хэндлеров
 
 
 async def on_startup(_):
     await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен')
 
+
+async def on_shutdown(_):
+    await storage.close()
+    await bot.send_message(chat_id=ADMIN_ID, text='Бот выключен')
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
