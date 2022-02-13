c301df0d3ed771b14f17b6a8f082c079139ba488
houlden <houlden105350@gmail.com>
2022-02-13 17:20:05 +0300

    added webhook

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from bot_init import bot, dp, storage
 from aiogram import executor
 from config import ADMIN_ID
@@ -6,12 +8,24 @@ import handlers  # Запуск хэндлеров
 
 async def on_startup(_):
     await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен')
+    await bot.set_webhook()
 
 
 async def on_shutdown(_):
-    await storage.close()
     await bot.send_message(chat_id=ADMIN_ID, text='Бот выключен')
+    await storage.close()
+    await bot.delete_webhook()
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    # executor.start_polling(dispatcher=dp,
+    #                        on_startup=on_startup,
+    #                        on_shutdown=on_shutdown,
+    #                        skip_updates=True)
+    executor.start_webhook(dispatcher=dp,
+                           webhook_path='',
+                           on_startup=on_startup,
+                           on_shutdown=on_shutdown,
+                           skip_updates=True,
+                           host='0.0.0.0',
+                           port=int(os.environ.get('PORT', 5000)))
